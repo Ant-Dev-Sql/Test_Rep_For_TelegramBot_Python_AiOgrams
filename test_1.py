8ffc1b5a0b9d4bb122f633907f5d7548e6d14d44
Ant <barkov.anton@gmail.com>
2025-02-21 09:36:59 +0700

    This is fifth change from local rep. with SSH with Changed config file into ~/.ssh/

--- test_1.py
+++ test_1.py
@@ -4,4 +4,5 @@ for i in range(0, 11):
   print('Test_Rep_For_TelegramBot_Python_AiOgrams')
   print('This is second change')
   print('This is third change from local rep.')
-  print('This is fourth change from local rep. with SSH')
+  print('This is fourth change from local rep. with SSH (didnt work)')
+  print('This is fifth change from local rep. with SSH with Changed config file into ~/.ssh/')
